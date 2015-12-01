ad4ffbdd04b206beb3fdb318782cff2a60f89ce0
S3b0 <Sebastian.Iffland@gmx.de>
2015-12-01 15:47:58 +0100

    Removed Model User

--- ext_tables.sql
+++ ext_tables.sql
@@ -42,13 +42,3 @@ CREATE TABLE tt_content (
 	tx_add_link_label varchar(255) DEFAULT '' NOT NULL,
 	tx_add_link_product varchar(255) DEFAULT '' NOT NULL
 );
-
-#
-# Table structure for table 'fe_users'
-#
-CREATE TABLE fe_users (
-	gender int(11) default '-1',
-	ecom_toolbox_country int(11) unsigned DEFAULT '0',
-	ecom_toolbox_state int(11) unsigned DEFAULT '0',
-	privacy_policy tinyint(4) unsigned DEFAULT '0' NOT NULL
-);
